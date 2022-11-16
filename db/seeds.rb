8845b860807fec2dc192ab32807f37fc6c9bb1e9
Bruno Allevato <brunoallevato@gmail.com>
2022-11-16 01:39:30 +0000

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,22 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+# User.create!(
+#   birthday: "14/09/1990",
+#   age: 32,
+#   sex: "male",
+#   height: rand(170..190),
+#   email: "bruno@all.com",
+#   password: "123456",
+#   password_confirmation: "123456",
+#   phone: "1234567",
+#   first_name: "Bruno",
+#   last_name: "Allevato"
+# )
+
 (1..30).each do |i|
   Weight.create!(
-    user_id: 1,
+    user_id: 3,
     weight: rand(60..100),
     created_at: DateTime.now + i,
     updated_at: DateTime.now + i
