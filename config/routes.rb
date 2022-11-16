73be36834c85fb0ac85a270ee72b22656b7fc655
Bruno Allevato <brunoallevato@gmail.com>
2022-11-16 21:11:05 +0000

    ajustes preview

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  
+
   resource :profiles, only: %i[show]
   resources :weights, only: %i[new create]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
