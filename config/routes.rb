fa9d94db1d2e93abcc531ca149358516cb9894d0
Felippe Oka <felippe.oka@gmail.com>
2022-11-22 22:29:52 -0300

    full profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :users do
+    get "/full_profile", to: "profiles#full_profile"
+    patch "/full_profile", to: "profiles#full_profile_edit"
+  end
+
   root to: "pages#home"
 
   resource :profiles, only: %i[show]
