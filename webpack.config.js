670957c217c1ff69e1b5320b016837629f86a0ac
Bruno Allevato <brunoallevato@gmail.com>
2022-11-23 18:47:49 +0000

    performance webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const webpack = require("webpack")
 
 module.exports = {
   mode: "production",
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
   devtool: "source-map",
   entry: {
     application: "./app/javascript/application.js"
