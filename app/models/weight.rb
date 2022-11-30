9a1d2e08d4e131709ec34cb9b699e0d2ffc3e90b
Bruno Allevato <brunoallevato@gmail.com>
2022-11-30 01:38:51 +0000

    weight index rows fix

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -2,7 +2,7 @@ class Weight < ApplicationRecord
   include PgSearch::Model
 
   pg_search_scope :search_by_value_and_created_at,
-                  against: %i[value created_at],
+                  against: :value,
                   using: {
                     tsearch: { prefix: true }
                   }
